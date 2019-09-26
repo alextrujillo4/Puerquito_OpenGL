baecc54170e25c303324b109b1a31a8007fdad12
Alex Trujillo <a01328426@itesm.mx>
2019-09-26 17:21:38 -0500

    vertices

--- playground.cpp
+++ playground.cpp
@@ -1,62 +1,98 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <GL/glew.h>
 #include <glfw3.h>
-#include <glm/glm.hpp>
-using namespace glm;
 
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
 
-int main( void ){
-    GLFWwindow* window;
 
-    // Initialise GLFW library
-    if( !glfwInit() ){
-        return -1;
-    }
-    window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Puerquito Project", NULL, NULL);
-    if( window == NULL ){
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
-    float vertices[] =
-    {
-        0, 300, 0.0, //Top LEFT Cornet
-         300, 300 ,0.0, //Top RIGHT Cornet
-         300 ,0 ,0.0, //Bottom LEFT corner
-         0, 0,0.0 //Bottom RIGHT corner
+   GLfloat point_edges_body[] = {
+       0,1,
+       0,3,
+       0,4,
+       2,1,
+       2,3,
+       2,7,
+       6,3,
+       6,4,
+       6,7,
+       5,1,
+       5,4,
+       5,7
+   };
+
+
+    GLfloat vertices_body[] = {
+        300, -300, 0,
+        300, 225, 0,
+        -300, 225, 0,
+        -300, -300, 0,
+        300, -300, 0,
+        300, 225, 900,
+        -300, -300, 900,
+        -300, 225,900 ,
     };
-    /*Normalize square pixels with screen deep*/
-    glViewport(0.0f, 0.0f, SCREEN_WIDTH, SCREEN_HEIGHT);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glOrtho(0, SCREEN_WIDTH, 0, SCREEN_HEIGHT, 0, 1);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-    /*END Normalize square pixels with screen deep*/
-    while (!glfwWindowShouldClose(window)) {
-        glClear(GL_COLOR_BUFFER_BIT);
-        /*
-          ====== RENDER OPEN GLE HERE==========
-         */
-        //TellOpenGLWeAreGonnaDrawVertices:
-        glEnableClientState(GL_VERTEX_ARRAY);
-        //glVertexPointer -> #OfDimension, Datatype, GAP, CreatedVariable
-        glVertexPointer(3, GL_FLOAT, 0, vertices);
-        //glDrawArrays -> FIGURE TYPE, VERTEX START, #ofTotalVertices
-        glDrawArrays(GL_QUADS, 0, 4);
-        glDisableClientState(GL_VERTEX_ARRAY);
-        /*
-        ====== END RENDER OPEN GLE HERE==========
-        */
-        //Swap front and back buffers
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-        
+
+
+
+int main( void )
+{
+    GLFWwindow *window;
+    
+    // Initialize the library
+    if ( !glfwInit( ) )
+    {
+        return -1;
     }
-    glfwTerminate();
+    
+    // Create a windowed mode window and its OpenGL context
+    window = glfwCreateWindow( SCREEN_WIDTH, SCREEN_HEIGHT, "Hello World", NULL, NULL );
+    
+    if ( !window )
+    {
+        glfwTerminate( );
+        return -1;
+    }
+    
+    // Make the window's context current
+    glfwMakeContextCurrent( window );
+    
+    glViewport( 0.0f, 0.0f, SCREEN_WIDTH, SCREEN_HEIGHT ); // specifies the part of the window to which OpenGL will draw (in pixels), convert from normalised to pixels
+    glMatrixMode( GL_PROJECTION ); // projection matrix defines the properties of the camera that views the objects in the world coordinate frame. Here you typically set the zoom factor, aspect ratio and the near and far clipping planes
+    glLoadIdentity( ); // replace the current matrix with the identity matrix and starts us a fresh because matrix transforms such as glOrpho and glRotate cumulate, basically puts us at (0, 0, 0)
+    glOrtho( -(SCREEN_WIDTH), SCREEN_WIDTH, -(SCREEN_HEIGHT), SCREEN_HEIGHT, 0, 1 ); // essentially set coordinate system
+    glMatrixMode( GL_MODELVIEW ); // (default matrix mode) modelview matrix defines how your objects are transformed (meaning translation, rotation and scaling) in your world
+    glLoadIdentity( ); // same as above comment
+    
+
+    
+    // Loop until the user closes the window
+    while ( !glfwWindowShouldClose( window ) )
+    {
+        glClear( GL_COLOR_BUFFER_BIT );
+        
+        // Render OpenGL here
+        glEnable( GL_LINE_SMOOTH );
+        glEnable( GL_LINE_STIPPLE );
+        glPushAttrib( GL_LINE_BIT );
+        glLineWidth( 10 );
+        glLineStipple( 1, 0x00FF );
+        glEnableClientState( GL_VERTEX_ARRAY );
+        glVertexPointer( 3, GL_FLOAT, 0, vertices_body );
+        glDrawArrays( GL_LINES, 0, 8 );
+        glDisableClientState( GL_VERTEX_ARRAY );
+        glPopAttrib( );
+        glDisable( GL_LINE_STIPPLE );
+        glDisable( GL_LINE_SMOOTH );
+        
+        
+        // Swap front and back buffers
+        glfwSwapBuffers( window );
+        
+        // Poll for and process events
+        glfwPollEvents( );
+    }
+    
+    glfwTerminate( );
+    
     return 0;
 }
-
